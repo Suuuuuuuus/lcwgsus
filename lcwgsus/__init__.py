f6469173f44405f727f7cc4e834a2626b39f30e3
Suuuuuuuus <bjzzp01@163.com>
2024-03-06 17:09:38 +0000

    Fixation

--- lcwgsus/__init__.py
+++
@@ -1,9 +0,0 @@
-# Import lcwgsus objects
-from .auxiliary import *  
-from .calculate import *  
-from .plot import *  
-from .read import * 
-from .save import *
-from .process import *
-
-__version__ = "0.0.1"
