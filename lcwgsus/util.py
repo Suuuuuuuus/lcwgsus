91a1a824f2f9814b0e92d518e57e93f8db482bf1
Suuuuuuuus <bjzzp01@163.com>
2024-04-18 17:01:41 +0100

    f

--- lcwgsus/util.py
+++ lcwgsus/util.py
@@ -29,7 +29,7 @@ from .process import *
 from .read import *
 from .plot import *
 
-__all__ = ["visualise_single_variant"]
+__all__ = ["visualise_single_variant", "visualise_single_variant_v2"]
 
 def visualise_single_variant(c, pos, vcf_lst, source_lst, labels_lst, vcf_cols = [
     'chr', 'pos', 'ID', 'ref', 'alt', 'QUAL', 'FILTER', 'INFO', 'FORMAT'
@@ -61,4 +61,46 @@ def visualise_single_variant(c, pos, vcf_lst, source_lst, labels_lst, vcf_cols =
     res = combine_violins(df_ary, labels_lst)
 
     plot_violin(res, x = 'GT', y = 'GP', hue = 'label', title = site, save_fig = save_fig, outdir = outdir, save_name = save_name)
+    return None
+
+def visualise_single_variant_v2(c, pos, vcf_lst, source_lst, labels_lst, vcf_cols = [
+    'chr', 'pos', 'ID', 'ref', 'alt', 'QUAL', 'FILTER', 'INFO', 'FORMAT'
+], mini = False, save_fig = False, outdir = None, save_name = None):
+    site = 'chr' + str(c) + ':' + str(pos) + '-' + str(pos)
+    df_ary = []
+    n = len(vcf_lst)
+    rename_map = generate_rename_map(mini = mini)
+
+    for i in vcf_lst:
+        command = "tabix" + " " + i + " " + site + " | head -n 1"
+        data = subprocess.run(command, shell = True, capture_output = True, text = True).stdout[:-1].split('\t')
+        command = "bcftools query -l" + " " + i
+        name = subprocess.run(command, shell = True, capture_output = True, text = True).stdout[:-1].split('\n')
+        col = vcf_cols + name
+        df = pd.DataFrame([data], columns=col)
+        df_ary.append(df)
+        
+    df_ary = resolve_common_samples(df_ary, source_lst, rename_map)
+    
+    df_ary[0] = df_ary[0].apply(extract_GT, axis = 1)
+    df_ary[0] = df_ary[0].drop(columns = vcf_cols)
+    df_ary[0] = df_ary[0].T.rename(columns = {0: 'GT'})
+
+    res_ary = []
+
+    for i in range(1, n):
+        if 'DS' in df_ary[i].loc[0, 'FORMAT']:
+            df_ary[i] = df_ary[i].apply(extract_DS, axis=1)
+        else:
+            df_ary[i] = df_ary[i].apply(extract_LDS_to_DS, axis=1)
+        
+        df_ary[i] = df_ary[i].drop(columns = vcf_cols)
+        df_ary[i] = df_ary[i].T.rename(columns = {0: 'DS'})
+        res = pd.merge(df_ary[i].reset_index(), df_ary[0].reset_index(), on = 'index').drop(columns = ['index'])
+        res['label'] = labels_lst[i]
+        res_ary.append(res)
+        
+    res = pd.concat(res_ary)
+
+    plot_violin(res, x = 'GT', y = 'DS', hue = 'label', title = site, save_fig = save_fig, outdir = outdir, save_name = save_name)
     return None
